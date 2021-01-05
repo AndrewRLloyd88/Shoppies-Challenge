af88d438d98c6151dfd9386b82d8216ddc843cdd
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-01-05 12:37:39 -0800

    add material-ui as dependency and change class-based react to functional

--- shoppies-frontend/src/App.js
+++ shoppies-frontend/src/App.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+export default function App() {
+  const [message, setMessage] = useState('Click to fetch data!');
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+  const fetchData = () => {
+    axios
+      .get('/api/data') // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
+        console.log(response.data.message); // Just the message
+        setMessage(response.data.message);
       });
-    }) 
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
+  return (
+    <div class="container mw-100">
+      <div>
+        <h1>{message}</h1>
+        <button onClick={fetchData}>Fetch Data</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
