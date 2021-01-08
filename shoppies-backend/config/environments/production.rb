0296d7f4f3013ab6daaf18eabb1af45dd3abfcfe
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-01-08 12:10:35 -0800

    upgrade ruby to v3.0.0 and rails to v6

--- shoppies-backend/config/environments/production.rb
+++ shoppies-backend/config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fork-it-all-backend_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "shoppies_backend_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
