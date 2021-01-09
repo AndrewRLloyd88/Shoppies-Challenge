7abeb77858b1c369675f616ab3c9b358daed2059
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-01-08 17:52:56 -0800

    start to implement access token feature

--- shoppies-frontend/src/App.js
+++ shoppies-frontend/src/App.js
@@ -1,22 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './styles/App.css';
 import LiveSearch from './components/LiveSearch';
+import uuid from 'react-uuid';
 
 export default function App() {
-  // const [message, setMessage] = useState('Click to fetch data!');
+  const [state, setState] = useState({
+    loggedInStatus: 'NOT_LOGGED_IN',
+    user: {},
+  });
 
-  // const fetchData = () => {
-  //   axios
-  //     .get('/api/data') // You can simply make your requests to "/api/whatever you want"
-  //     .then((response) => {
-  //       // handle success
-  //       console.log(response.data); // The entire response from the Rails API
+  const checkLoginStatus = () => {
+    axios
+      .get('/api/logged_in', {
+        headers: {
+          authorization: `Token token=${localStorage.getItem('access_token')}`,
+        },
+        withCredentials: false,
+      })
+      .then((response) => {
+        if (
+          response.data.logged_in &&
+          state.loggedInStatus === 'NOT_LOGGED_IN'
+        ) {
+          setState({
+            loggedInStatus: 'LOGGED_IN',
+            user: response.data.user,
+          });
+        } else if (
+          !response.data.logged_in &&
+          state.loggedInStatus === 'LOGGED_IN'
+        ) {
+          setState({
+            loggedInStatus: 'NOT_LOGGED_IN',
+            user: {},
+          });
+        }
+      })
+      .catch((error) => {
+        console.log('Explosions! ', error);
+      });
+  };
+  useEffect(() => {
+    checkLoginStatus();
+  });
 
-  //       console.log(response.data.message); // Just the message
-  //       setMessage(response.data.message);
-  //     });
-  // };
+  const handleLogin = (data) => {
+    localStorage.setItem('access_token', data.user.access_token);
+    setState({
+      loggedInStatus: 'LOGGED_IN',
+      user: data.user,
+    });
+  };
+
+  const createUser = () => {
+    axios
+      .post(
+        '/api/registrations',
+        {
+          slug: uuid(),
+        },
+        {
+          headers: {
+            authorization: `Token token=${localStorage.getItem(
+              'access_token'
+            )}`,
+          },
+        },
+        { withCredentials: false }
+      )
+      .then((response) => {
+        if (response.data.status === 'created') {
+          localStorage.setItem('access_token', response.data.user.access_token);
+          setState({
+            loggedInStatus: 'LOGGED_IN',
+            slug: response.data.slug,
+          });
+          console.log(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log('Error: ', error);
+      });
+  };
+
+  // useEffect(() => {
+  //   createUser();
+  // }, []);
 
   return <LiveSearch />;
 }
